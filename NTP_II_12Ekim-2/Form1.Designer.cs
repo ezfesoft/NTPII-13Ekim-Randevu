4adb9326084bfc76329d5849f704190291ccf365
slymn <slymn@SLYMNEB4D>
2023-10-20 11:57:43 +0300

    test

---
+++ NTP_II_12Ekim-2/Form1.Designer.cs
@@ -0,0 +1,358 @@
+﻿namespace NTP_II_12Ekim_2
+{
+    partial class Form1
+    {
+        /// <summary>
+        ///Gerekli tasarımcı değişkeni.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///Kullanılan tüm kaynakları temizleyin.
+        /// </summary>
+        ///<param name="disposing">yönetilen kaynaklar dispose edilmeliyse doğru; aksi halde yanlış.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer üretilen kod
+
+        /// <summary>
+        /// Tasarımcı desteği için gerekli metot - bu metodun 
+        ///içeriğini kod düzenleyici ile değiştirmeyin.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.tabControl1 = new System.Windows.Forms.TabControl();
+            this.tabPage1 = new System.Windows.Forms.TabPage();
+            this.tabPage2 = new System.Windows.Forms.TabPage();
+            this.label1 = new System.Windows.Forms.Label();
+            this.listBox1 = new System.Windows.Forms.ListBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.textBox1TC = new System.Windows.Forms.TextBox();
+            this.textBox1Ad = new System.Windows.Forms.TextBox();
+            this.textBox1Soyad = new System.Windows.Forms.TextBox();
+            this.label4 = new System.Windows.Forms.Label();
+            this.button1 = new System.Windows.Forms.Button();
+            this.listBox2 = new System.Windows.Forms.ListBox();
+            this.label5 = new System.Windows.Forms.Label();
+            this.textBox2Soyad = new System.Windows.Forms.TextBox();
+            this.label6 = new System.Windows.Forms.Label();
+            this.textBox2Ad = new System.Windows.Forms.TextBox();
+            this.textBox2TC = new System.Windows.Forms.TextBox();
+            this.label7 = new System.Windows.Forms.Label();
+            this.label8 = new System.Windows.Forms.Label();
+            this.textBox2Saat = new System.Windows.Forms.TextBox();
+            this.label9 = new System.Windows.Forms.Label();
+            this.listBox3 = new System.Windows.Forms.ListBox();
+            this.tabControl1.SuspendLayout();
+            this.tabPage1.SuspendLayout();
+            this.tabPage2.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // tabControl1
+            // 
+            this.tabControl1.Controls.Add(this.tabPage1);
+            this.tabControl1.Controls.Add(this.tabPage2);
+            this.tabControl1.Location = new System.Drawing.Point(12, 12);
+            this.tabControl1.Name = "tabControl1";
+            this.tabControl1.SelectedIndex = 0;
+            this.tabControl1.Size = new System.Drawing.Size(362, 426);
+            this.tabControl1.TabIndex = 0;
+            this.tabControl1.SelectedIndexChanged += new System.EventHandler(this.tabControl1_SelectedIndexChanged);
+            // 
+            // tabPage1
+            // 
+            this.tabPage1.Controls.Add(this.button1);
+            this.tabPage1.Controls.Add(this.textBox1Soyad);
+            this.tabPage1.Controls.Add(this.label4);
+            this.tabPage1.Controls.Add(this.textBox1Ad);
+            this.tabPage1.Controls.Add(this.textBox1TC);
+            this.tabPage1.Controls.Add(this.label3);
+            this.tabPage1.Controls.Add(this.label2);
+            this.tabPage1.Controls.Add(this.listBox1);
+            this.tabPage1.Controls.Add(this.label1);
+            this.tabPage1.Location = new System.Drawing.Point(4, 22);
+            this.tabPage1.Name = "tabPage1";
+            this.tabPage1.Padding = new System.Windows.Forms.Padding(3);
+            this.tabPage1.Size = new System.Drawing.Size(354, 400);
+            this.tabPage1.TabIndex = 0;
+            this.tabPage1.Text = "Randevu Alma Sayfası";
+            this.tabPage1.UseVisualStyleBackColor = true;
+            // 
+            // tabPage2
+            // 
+            this.tabPage2.Controls.Add(this.textBox2Saat);
+            this.tabPage2.Controls.Add(this.label9);
+            this.tabPage2.Controls.Add(this.textBox2Soyad);
+            this.tabPage2.Controls.Add(this.label6);
+            this.tabPage2.Controls.Add(this.textBox2Ad);
+            this.tabPage2.Controls.Add(this.textBox2TC);
+            this.tabPage2.Controls.Add(this.label7);
+            this.tabPage2.Controls.Add(this.label8);
+            this.tabPage2.Controls.Add(this.label5);
+            this.tabPage2.Controls.Add(this.listBox2);
+            this.tabPage2.Location = new System.Drawing.Point(4, 22);
+            this.tabPage2.Name = "tabPage2";
+            this.tabPage2.Padding = new System.Windows.Forms.Padding(3);
+            this.tabPage2.Size = new System.Drawing.Size(354, 400);
+            this.tabPage2.TabIndex = 1;
+            this.tabPage2.Text = "Randevu Detay Sayfası";
+            this.tabPage2.UseVisualStyleBackColor = true;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 13F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.label1.Location = new System.Drawing.Point(6, 9);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(148, 22);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Randevu Saatleri";
+            // 
+            // listBox1
+            // 
+            this.listBox1.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.listBox1.FormattingEnabled = true;
+            this.listBox1.ItemHeight = 20;
+            this.listBox1.Items.AddRange(new object[] {
+            "09:00 - 09:15",
+            "09:15 - 09:30",
+            "09:30 - 09:45",
+            "09:45 - 10:00",
+            "10:00 - 10:15",
+            "10:15 - 10:30",
+            "10:30 - 10:45",
+            "10:45 - 11:00",
+            "11:00 - 11:15",
+            "11:15 - 11:30",
+            "11:30 - 11:45",
+            "11:45 - 12:00"});
+            this.listBox1.Location = new System.Drawing.Point(6, 34);
+            this.listBox1.Name = "listBox1";
+            this.listBox1.Size = new System.Drawing.Size(148, 344);
+            this.listBox1.TabIndex = 1;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.label2.Location = new System.Drawing.Point(160, 34);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(58, 20);
+            this.label2.TabIndex = 2;
+            this.label2.Text = "TC No";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.label3.Location = new System.Drawing.Point(160, 92);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(31, 20);
+            this.label3.TabIndex = 3;
+            this.label3.Text = "Ad";
+            // 
+            // textBox1TC
+            // 
+            this.textBox1TC.Font = new System.Drawing.Font("Microsoft Sans Serif", 15F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.textBox1TC.Location = new System.Drawing.Point(164, 57);
+            this.textBox1TC.Name = "textBox1TC";
+            this.textBox1TC.Size = new System.Drawing.Size(157, 30);
+            this.textBox1TC.TabIndex = 4;
+            // 
+            // textBox1Ad
+            // 
+            this.textBox1Ad.Font = new System.Drawing.Font("Microsoft Sans Serif", 15F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.textBox1Ad.Location = new System.Drawing.Point(164, 115);
+            this.textBox1Ad.Name = "textBox1Ad";
+            this.textBox1Ad.Size = new System.Drawing.Size(157, 30);
+            this.textBox1Ad.TabIndex = 5;
+            // 
+            // textBox1Soyad
+            // 
+            this.textBox1Soyad.Font = new System.Drawing.Font("Microsoft Sans Serif", 15F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.textBox1Soyad.Location = new System.Drawing.Point(164, 183);
+            this.textBox1Soyad.Name = "textBox1Soyad";
+            this.textBox1Soyad.Size = new System.Drawing.Size(157, 30);
+            this.textBox1Soyad.TabIndex = 7;
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.label4.Location = new System.Drawing.Point(160, 160);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(59, 20);
+            this.label4.TabIndex = 6;
+            this.label4.Text = "Soyad";
+            // 
+            // button1
+            // 
+            this.button1.Font = new System.Drawing.Font("Microsoft Sans Serif", 15F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.button1.Location = new System.Drawing.Point(164, 221);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(157, 157);
+            this.button1.TabIndex = 8;
+            this.button1.Text = "Randevu Al";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // listBox2
+            // 
+            this.listBox2.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.listBox2.FormattingEnabled = true;
+            this.listBox2.ItemHeight = 20;
+            this.listBox2.Location = new System.Drawing.Point(200, 50);
+            this.listBox2.Name = "listBox2";
+            this.listBox2.Size = new System.Drawing.Size(148, 344);
+            this.listBox2.TabIndex = 2;
+            this.listBox2.SelectedIndexChanged += new System.EventHandler(this.listBox2_SelectedIndexChanged);
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Font = new System.Drawing.Font("Microsoft Sans Serif", 13F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.label5.Location = new System.Drawing.Point(200, 25);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(148, 22);
+            this.label5.TabIndex = 3;
+            this.label5.Text = "Randevu Saatleri";
+            // 
+            // textBox2Soyad
+            // 
+            this.textBox2Soyad.Enabled = false;
+            this.textBox2Soyad.Font = new System.Drawing.Font("Microsoft Sans Serif", 15F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.textBox2Soyad.Location = new System.Drawing.Point(10, 176);
+            this.textBox2Soyad.Name = "textBox2Soyad";
+            this.textBox2Soyad.Size = new System.Drawing.Size(157, 30);
+            this.textBox2Soyad.TabIndex = 13;
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.label6.Location = new System.Drawing.Point(6, 153);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(59, 20);
+            this.label6.TabIndex = 12;
+            this.label6.Text = "Soyad";
+            // 
+            // textBox2Ad
+            // 
+            this.textBox2Ad.Enabled = false;
+            this.textBox2Ad.Font = new System.Drawing.Font("Microsoft Sans Serif", 15F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.textBox2Ad.Location = new System.Drawing.Point(10, 108);
+            this.textBox2Ad.Name = "textBox2Ad";
+            this.textBox2Ad.Size = new System.Drawing.Size(157, 30);
+            this.textBox2Ad.TabIndex = 11;
+            // 
+            // textBox2TC
+            // 
+            this.textBox2TC.Enabled = false;
+            this.textBox2TC.Font = new System.Drawing.Font("Microsoft Sans Serif", 15F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.textBox2TC.Location = new System.Drawing.Point(10, 50);
+            this.textBox2TC.Name = "textBox2TC";
+            this.textBox2TC.Size = new System.Drawing.Size(157, 30);
+            this.textBox2TC.TabIndex = 10;
+            // 
+            // label7
+            // 
+            this.label7.AutoSize = true;
+            this.label7.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.label7.Location = new System.Drawing.Point(6, 85);
+            this.label7.Name = "label7";
+            this.label7.Size = new System.Drawing.Size(31, 20);
+            this.label7.TabIndex = 9;
+            this.label7.Text = "Ad";
+            // 
+            // label8
+            // 
+            this.label8.AutoSize = true;
+            this.label8.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.label8.Location = new System.Drawing.Point(6, 27);
+            this.label8.Name = "label8";
+            this.label8.Size = new System.Drawing.Size(58, 20);
+            this.label8.TabIndex = 8;
+            this.label8.Text = "TC No";
+            // 
+            // textBox2Saat
+            // 
+            this.textBox2Saat.Enabled = false;
+            this.textBox2Saat.Font = new System.Drawing.Font("Microsoft Sans Serif", 15F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.textBox2Saat.Location = new System.Drawing.Point(10, 241);
+            this.textBox2Saat.Name = "textBox2Saat";
+            this.textBox2Saat.Size = new System.Drawing.Size(157, 30);
+            this.textBox2Saat.TabIndex = 15;
+            // 
+            // label9
+            // 
+            this.label9.AutoSize = true;
+            this.label9.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.label9.Location = new System.Drawing.Point(6, 218);
+            this.label9.Name = "label9";
+            this.label9.Size = new System.Drawing.Size(47, 20);
+            this.label9.TabIndex = 14;
+            this.label9.Text = "Saat";
+            // 
+            // listBox3
+            // 
+            this.listBox3.FormattingEnabled = true;
+            this.listBox3.Location = new System.Drawing.Point(380, 34);
+            this.listBox3.Name = "listBox3";
+            this.listBox3.Size = new System.Drawing.Size(245, 394);
+            this.listBox3.TabIndex = 1;
+            this.listBox3.Visible = false;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(425, 450);
+            this.Controls.Add(this.listBox3);
+            this.Controls.Add(this.tabControl1);
+            this.Name = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.tabControl1.ResumeLayout(false);
+            this.tabPage1.ResumeLayout(false);
+            this.tabPage1.PerformLayout();
+            this.tabPage2.ResumeLayout(false);
+            this.tabPage2.PerformLayout();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TabControl tabControl1;
+        private System.Windows.Forms.TabPage tabPage1;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.TextBox textBox1Soyad;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.TextBox textBox1Ad;
+        private System.Windows.Forms.TextBox textBox1TC;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.ListBox listBox1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TabPage tabPage2;
+        private System.Windows.Forms.TextBox textBox2Saat;
+        private System.Windows.Forms.Label label9;
+        private System.Windows.Forms.TextBox textBox2Soyad;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.TextBox textBox2Ad;
+        private System.Windows.Forms.TextBox textBox2TC;
+        private System.Windows.Forms.Label label7;
+        private System.Windows.Forms.Label label8;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.ListBox listBox2;
+        private System.Windows.Forms.ListBox listBox3;
+    }
+}
+
---
+++ NTP_II_12Ekim-2/Form1.cs
@@ -0,0 +1,114 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace NTP_II_12Ekim_2
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            listBox1.SelectedIndex = 0;
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            string _saat = "";
+            int _tc ;
+            string _ad = "";
+            string _soyad = "";
+            
+            _saat = listBox1.SelectedItem.ToString();
+            _tc = Int32.Parse(textBox1TC.Text);
+            _ad = textBox1Ad.Text;
+            _soyad = textBox1Soyad.Text;
+
+            int _seciliIndexNo;
+            _seciliIndexNo = listBox1.SelectedIndex;
+
+            string _TekSatir;
+
+
+
+            bool _kayitDurumu = false;
+            string[] dizi;
+            for (int i = 0; i < listBox3.Items.Count; i++)
+            {
+                dizi = listBox3.Items[i].ToString().Split(';');
+                if (_tc.ToString() == dizi[0])
+                {
+                    _kayitDurumu = true;
+                    MessageBox.Show("Zaten Kayıt Yapıldı!");
+                    textBox1TC.Text = "";
+                    textBox1Ad.Text = "";
+                    textBox1Soyad.Text="";
+                    listBox1.SelectedIndex = 0;
+                }
+            }
+            if (_kayitDurumu==false)
+            {
+                if (_tc % 2 == 0 && _tc.ToString().Length > 3)
+                {
+                    if (_ad.Length >= 2)
+                    {
+                        if (_soyad.Length >= 2)
+                        {
+                            _TekSatir = _tc + ";" + _ad + ";" + _soyad + ";" + _saat;
+                            listBox3.Items.Add(_TekSatir);
+                            listBox1.Items.RemoveAt(_seciliIndexNo);
+                            textBox1TC.Text = "";
+                            textBox1Ad.Text = "";
+                            textBox1Soyad.Text = "";
+                            listBox1.SelectedIndex = 0;
+                        }
+                    }
+                }
+                else
+                {
+                    MessageBox.Show("Geçerli TC Giriniz!");
+
+                    textBox1TC.Text = "";
+                    textBox1Ad.Text = "";
+                    textBox1Soyad.Text = "";
+                    listBox1.SelectedIndex = 0;
+                }
+            }
+        }
+
+        private void tabControl1_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            int _list3sayi = listBox3.Items.Count;
+            string[] dizi;
+            for (int i = 0; i < _list3sayi; i++)
+            {
+                dizi = listBox3.Items[i].ToString().Split(';');
+                listBox2.Items.Add(dizi[3]);
+            }
+        }
+
+        private void listBox2_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            int _list2Index = listBox2.SelectedIndex;
+
+            string[] dizi;
+            dizi = listBox3.Items[_list2Index].ToString().Split(';');
+
+            textBox2TC.Text = dizi[0];
+            textBox2Ad.Text = dizi[1];
+            textBox2Soyad.Text = dizi[2];
+            textBox2Saat.Text = dizi[3];
+
+        }
+    }
+}
